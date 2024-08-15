aceeae7776c0ee90d18e3075419ff198e4dd915d
Vladislav Moskalenko <62288939+vlad19930514@users.noreply.github.com>
2024-08-15 21:10:53 +0300

    user service

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/golang-migrate/migrate/v4"
 	"github.com/rs/zerolog"
 	"os"
 	"time"
@@ -15,7 +14,6 @@ import (
 	pg "github.com/vlad19930514/webApp/internal/pkg/pg"
 	"github.com/vlad19930514/webApp/util"
 
-	"errors"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
@@ -47,48 +45,12 @@ func runApplication() error {
 	}
 	// create services
 	userService := services.NewUserService(userRepo)
-	//run migration
-	/*err = runDBMigration(config.MigrationURL, config.DBSource)
-	if err != nil {
-		return fmt.Errorf("cannot migrate: %w", err)
-	}*/
 
 	server := httpserver.NewHttpServer(userService)
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
 		return fmt.Errorf("cannot start server: %w", err)
-	}
-	return nil
-}
-
-func runDBMigration(migrationURL string, dbSource string) error {
-	log.Info().Str("migrationURL", migrationURL).Str("dbSource", dbSource).Msg("starting DB migration")
-
-	migration, err := migrate.New(migrationURL, dbSource)
-	if err != nil {
-		return fmt.Errorf("cannot create new migrate instance: %w", err)
-	}
-	defer func() {
-		sourceErr, databaseErr := migration.Close()
-		if sourceErr != nil {
-			log.Error().Err(sourceErr).Msg("error closing migration source")
-		}
-		if databaseErr != nil {
-			log.Error().Err(databaseErr).Msg("error closing migration database")
-		}
-
-	}()
-
-	if err = migration.Up(); err != nil {
-		if errors.Is(err, migrate.ErrNoChange) {
-			log.Info().Msg("no change in migrations")
-
-		} else {
-			log.Fatal().Err(err).Msg("failed to run migrate up")
-		}
-	} else {
-		log.Info().Msg("db migrated successfully")
 	}
 	return nil
 }
